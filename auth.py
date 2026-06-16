a2d7f267ef4cb35f83c0aabda9c3672a3dd81767
Efe Raphael OBIBI <efeobibi@gmail.com>
2026-06-16 12:18:18 +0100

    updated signup route

--- auth.py
+++ auth.py
@@ -15,4 +15,7 @@ def login(u_name, p_word):
 def signup(u_name, p_word, p_word2):
     if p_word != p_word2:
         print("The two password do not match!")
-
+    elif u_name in database.keys():
+        print("This username is taken")
+    else:
+        database[u_name] = p_word
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import auth
 
 username = "Raphael"
 password = "password123"
+password2 = "password123"
 
 auth.login(username, password)
+auth.signup(username, password, password2)
